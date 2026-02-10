99b67fa398a86f94e3b902588ad0993d97a11655
healer154 <eng.m.saad1997@gmail.com>
2026-02-10 22:42:09 +0200

    11_2

--- src/main.tsx
+++ src/main.tsx
@@ -3,4 +3,11 @@ import App from "./App.tsx";
 import "./index.css";
 import "./i18n";
 
-createRoot(document.getElementById("root")!).render(<App />);
+import { Analytics } from "@vercel/analytics/react";
+
+createRoot(document.getElementById("root")!).render(
+    <>
+        <App />
+        <Analytics />
+    </>
+);
